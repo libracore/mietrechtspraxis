60dabf0420ecddd1f4e6974b72e18abba288e26c
Joelios <joel.mesmer@libracore.com>
2022-07-14 15:04:47 +0200

    v1.2.1; Hotfix Verr. Vorbest. inkl. RG-Datum und sortiert nach anz Bücher

--- mietrechtspraxis/mietrechtspraxis/utils/so_to_sinv.py
+++ mietrechtspraxis/mietrechtspraxis/utils/so_to_sinv.py
@@ -20,27 +20,27 @@ def create_invoices(tag, posting_date):
                                     AND `per_billed` < 1
                                     ORDER BY `total_qty` DESC""".format(tag=tag), as_dict=True)
     if len(sales_orders) > 0:
-        sinvs = []
-        return_value = ''
-        for so in sales_orders:
-            sinv = frappe.get_doc(make_sales_invoice(so.name, target_doc=None, ignore_permissions=False))
-            sinv.set_posting_time = 1
-            sinv.posting_date = posting_date
-            sinv.insert()
-            sinv.esr_reference = get_qrr_reference(sales_invoice=sinv.name, customer=sinv.customer)
-            sinv.submit()
-            sinvs.append(sinv.name)
-            return_value = sinv.name
-        print_rechnungen(sinvs, return_value)
+        return_value = sales_orders[0].name
+        args = {
+            'sales_orders': sales_orders,
+            'posting_date': posting_date
+        }
+        enqueue("mietrechtspraxis.mietrechtspraxis.utils.so_to_sinv.create_rechnungen", queue='long', job_name=return_value, timeout=5000, **args)
         return return_value
     else:
         return 'keine'
 
-def print_rechnungen(sinvs, return_value):
-    args = {
-        'sinvs': sinvs
-    }
-    enqueue("mietrechtspraxis.mietrechtspraxis.utils.so_to_sinv.blk_print_rechnungen", queue='long', job_name=return_value, timeout=5000, **args)
+def create_rechnungen(sales_orders, posting_date):
+    sinvs = []
+    for so in sales_orders:
+        sinv = frappe.get_doc(make_sales_invoice(so.name, target_doc=None, ignore_permissions=False))
+        sinv.set_posting_time = 1
+        sinv.posting_date = posting_date
+        sinv.insert()
+        sinv.esr_reference = get_qrr_reference(sales_invoice=sinv.name, customer=sinv.customer)
+        sinv.submit()
+        sinvs.append(sinv.name)
+    blk_print_rechnungen(sinvs)
 
 def blk_print_rechnungen(sinvs):
     save_counter = 1
