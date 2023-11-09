d1201fa6c89764a4b63a8497fba488c7bd7e9210
Joelios <joel.mesmer@libracore.com>
2023-11-09 11:17:08 +0100

    mp shop api endpoint

--- mietrechtspraxis/api.py
+++ mietrechtspraxis/api.py
@@ -13,7 +13,75 @@ from mietrechtspraxis.mietrechtspraxis.doctype.arbitration_authority.arbitration
 @frappe.whitelist(allow_guest=True)
 def request(**kwargs):
     return _request(**kwargs)
-    
+
 @frappe.whitelist(allow_guest=True)
 def get_sb(**kwargs):
     return _get_sb(**kwargs)
+@frappe.whitelist(allow_guest=True)
+def mp_shop(**kwargs):
+    def get_mp_shop_item_groups():
+        item_groups = frappe.db.sql("""SELECT
+                                            `name` AS `item_group`,
+                                            `description` AS `description`
+                                        FROM `tabItem Group`
+                                        WHERE `show_in_website` = 1
+                                        ORDER BY `weightage` ASC
+                                    """, as_dict=True)
+        return item_groups
+    
+    def get_mp_shop_items(item_group):
+        items = frappe.db.sql("""SELECT
+                                    `name` AS `item_code`,
+                                    `item_name` AS `item_name`,
+                                    `web_long_description` AS `description`,
+                                    NULL AS `rate`,
+                                    `website_image` AS `image`
+                                FROM `tabItem`
+                                WHERE `show_in_website` = 1
+                                AND `name` IN (
+                                    SELECT
+                                        `parent`
+                                    FROM `tabWebsite Item Group`
+                                    WHERE `item_group` = '{item_group}'
+                                )
+                                ORDER BY `weightage` ASC
+                            """.format(item_group=item_group), as_dict=True)
+        for item in items:
+            item = get_mp_shop_rate(item)
+        
+        return items
+    
+    def get_mp_shop_rate(item):
+        rate = frappe.db.sql("""SELECT
+                                    `price_list_rate` AS `rate`
+                                FROM `tabItem Price`
+                                WHERE `item_code` = '{item_code}'
+                                AND `selling` = 1
+                                ORDER BY `price_list_rate` DESC
+                            """.format(item_code=item.item_code), as_dict=True)
+        
+        if len(rate) > 1:
+            rate = rate[0].rate
+            frappe.log_error("Mehere Preise für {item_code} gefunden!".format(item_code=item.item_code), "MP Shop API")
+        if len(rate) < 1:
+            rate = 0
+            frappe.log_error("Kein Preis für {item_code} gefunden!".format(item_code=item.item_code), "MP Shop API")
+        else:
+            rate = rate[0].rate
+        
+        item.rate = rate
+        return item
+    
+    data = []
+    item_groups = get_mp_shop_item_groups()
+    
+    for item_group in item_groups:
+        items = get_mp_shop_items(item_group.item_group)
+        
+        data.append({
+            'item_group': item_group.item_group,
+            'description': item_group.description,
+            'items': items
+        })
+    
+    return data
