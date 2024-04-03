10e2ad2d1f5af92e63525785d31dfbb4d2738326
joelios <joel.mesmer@libracore.com>
2024-04-03 11:36:10 +0200

    1.13.2

---
+++ mietrechtspraxis/public/js/custom_scripts/contact.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2021-2024, libracore AG and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Contact', {
+	refresh: function(frm) {
+        frm.fields_dict.mp_web_user.get_query = function(doc, cdt, cdn) {
+            return {
+                query: "frappe.core.doctype.user.user.user_query",
+                filters: {ignore_user_type: 1}
+            }
+        }
+        frm.refresh_field("mp_web_user");
+	}
+});
