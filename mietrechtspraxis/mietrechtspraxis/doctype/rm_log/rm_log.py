4b8c1928adf8a64c2ecc46021141e564faa96921
Joel <joel.mesmer@libracore.com>
2024-11-05 20:04:13 +0100

    bugfix

--- mietrechtspraxis/mietrechtspraxis/doctype/rm_log/rm_log.py
+++ mietrechtspraxis/mietrechtspraxis/doctype/rm_log/rm_log.py
@@ -15,10 +15,11 @@ def sinv_storno(rm_log):
     rm_log = frappe.get_doc("RM Log", rm_log)
     for rm_log_sinv in rm_log.sinvs:
         sinv = frappe.get_doc("Sales Invoice", rm_log_sinv.sinv)
-        sinv_in_abo = frappe.db.sql("""SELECT `name` FROM `tabmp Abo Invoice` WHERE `sales_invoice` = '{sinv}'""".format(sinv=sinv.name), as_dict=True)[0].name
-        delete_sinv_in_abo = frappe.db.sql("""DELETE FROM `tabmp Abo Invoice` WHERE `name` = '{sinv}'""".format(sinv=sinv_in_abo), as_list=True)
-        sinv.cancel()
-        frappe.db.commit()
+        if str(sinv.docstatus) != '2':
+            sinv_in_abo = frappe.db.sql("""SELECT `name` FROM `tabmp Abo Invoice` WHERE `sales_invoice` = '{sinv}'""".format(sinv=sinv.name), as_dict=True)[0].name
+            delete_sinv_in_abo = frappe.db.sql("""DELETE FROM `tabmp Abo Invoice` WHERE `name` = '{sinv}'""".format(sinv=sinv_in_abo), as_list=True)
+            sinv.cancel()
+            frappe.db.commit()
     
     bind_source = "/assets/mietrechtspraxis/sinvs_for_print/{date}.pdf".format(date=rm_log.name)
     physical_path = "/home/frappe/frappe-bench/sites" + bind_source
